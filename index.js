999c32807221bc17bafe92d5ac62fe1b83ea0990
dingchunlei <harley.mistyyyy@gmail.com>
2020-01-14 10:09:18 +0800

    fix the index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/big-numbers.min.js');
+  module.exports = require('./dist/lnam.min.js');
 } else {
-  module.exports = require('./dist/big-numbers.js');
+  module.exports = require('./dist/lnam.js');
 }
